46ced818ee1fb5808336c51c15a311245de3c700
Joyless <65855333+Joy-less@users.noreply.github.com>
2026-02-08 17:15:07 +0000

    Use let not var in syntaxHighlight

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -69,7 +69,7 @@ ${syntaxHighlight(previewJsonResult) }
 function syntaxHighlight(json: string): string {
 	json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
 	return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-		var cls = 'number';
+		let cls = 'number';
 		if (/^"/.test(match)) {
 			if (/:$/.test(match)) {
 				cls = 'key';
